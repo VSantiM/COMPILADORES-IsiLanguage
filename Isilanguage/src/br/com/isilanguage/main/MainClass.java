90eb4569d170038e8819c67c3efab2ed29fb4337
ErickHenrique2000 <erick.henrique@aluno.ufabc.edu.br>
2022-08-18 23:12:57 -0300

    mainclass start

--- Isilanguage/src/br/com/isilanguage/main/MainClass.java
+++ Isilanguage/src/br/com/isilanguage/main/MainClass.java
@@ -1,8 +1,28 @@
 package br.com.isilanguage.main;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import br.com.isilanguage.parser.IsiLangLexer;
+import br.com.isilanguage.parser.IsiLangParser;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
+		try {
+			IsiLangLexer lexer;
+			IsiLangParser parser;
+			
+			lexer = new IsiLangLexer(CharStreams.fromFileName("input.isi"));
+			
+			CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+			
+			parser = new IsiLangParser(tokenStream);
+			parser.prog();
+			
+			System.out.println("Compilation Successful");
+		}catch(Exception ex) {
+			System.out.println("ERROR "+ex.getMessage());
+		}
 	}
 }
